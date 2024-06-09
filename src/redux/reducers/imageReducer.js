fa3739c85af98e17425d9ea2f8ad6e0c548a3614
Nishant Thakur <theboolean.in@gmail.com>
2024-06-09 13:49:17 +0530

    images
    images

--- src/redux/reducers/imageReducer.js
+++ src/redux/reducers/imageReducer.js
@@ -1,4 +1,3 @@
-
 // const initialState = {
 //     images: [
 //         {
@@ -20,61 +19,63 @@
 //     ],
 // };
 // imageReducer.js
-import { FETCH_IMAGES_REQUEST, FETCH_IMAGES_SUCCESS, FETCH_IMAGES_FAILURE } from '../types';
+import {
+  FETCH_IMAGES_REQUEST,
+  FETCH_IMAGES_SUCCESS,
+  FETCH_IMAGES_FAILURE,
+} from "../types";
 
 const initialState = {
-    images: [
-        {
-            SceneName: "Entrance",
-            Images: {
-                ImageBase64: `/images/entrance.jpg`,
-                coOrdinates: [-30, -30],
-            },
-
-        },
-        {
-            SceneName: "Lobby",
-            Images: {
-                ImageBase64: `/images/lobby1.jpg`,
-                coOrdinates: [15, 25],
-            },
-
-        },{
-            SceneName: "Clinic",
-            Images: {
-                ImageBase64: `/images/img2.jpg`,
-                coOrdinates: [35, -90],
-            },
-
-        },
-    ],
-    loading: false,
-    error: null,
+  images: [
+    {
+      SceneName: "Entrance",
+      Images: {
+        ImageBase64: `/images/1.jpg`,
+        coOrdinates: [-30, -30],
+      },
+    },
+    {
+      SceneName: "Lobby",
+      Images: {
+        ImageBase64: `/images/lobby1.jpg`,
+        coOrdinates: [15, 25],
+      },
+    },
+    {
+      SceneName: "Clinic",
+      Images: {
+        ImageBase64: `/images/clinic.jpg`,
+        coOrdinates: [35, -90],
+      },
+    },
+  ],
+  loading: false,
+  error: null,
 };
 
 const imageReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_IMAGES_REQUEST:
-            return {
-                ...state,
-                loading: true,
-                error: null,
-            };
-        case FETCH_IMAGES_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                images: action.payload ? action.payload : initialState.images,
-            };
-        case FETCH_IMAGES_FAILURE:
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case FETCH_IMAGES_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_IMAGES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        images: action.payload ? action.payload : initialState.images,
+      };
+    case FETCH_IMAGES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default imageReducer;
